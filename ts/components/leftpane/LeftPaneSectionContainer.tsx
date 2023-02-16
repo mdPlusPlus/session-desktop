7783edf3e892d3220d8df490a8db43c3392a04c8
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2023-02-16 02:21:14 +0100

    Remove horizontal scrollbar from LeftPaneSectionContainer

--- ts/components/leftpane/LeftPaneSectionContainer.tsx
+++ ts/components/leftpane/LeftPaneSectionContainer.tsx
@@ -6,6 +6,7 @@ export const LeftPaneSectionContainer = styled.div`
   flex-direction: column;
   align-items: center;
   overflow-y: auto;
+  overflow-x: hidden;
 
   .session-icon-button {
     padding: 30px 20px;
